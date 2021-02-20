1bd9c27e9021616d89aa21557cb1edca3d37280d
Ricardo All <rick.dev27@gmail.com>
2021-02-19 18:55:48 -0600

    .

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -48,128 +48,127 @@ class About extends Component {
         >
           <div className="container">
             <div className="row">
-              <div className="col-12 col-md-1 col-sm-1 col-lg-1 col-xl-1"></div>
-
-              <dib className="col col-md-12 col-sm-12 col-lg col-xl">
-                <div className="row">
-                  <div className="col-12">
-                    <div className="card-body text-center">
-                      {/* <img
+              <div className="col-12">
+                <div className="card-body text-center">
+                  {/* <img
               id="imagAvatar"
               src={require("../../images/BeeingCreative5.png")}
               style={{ width: 50, height: 50, color: "#111111" }}
               className="avatar p-1  mb-4 "
             /> */}
-                      <img
-                        src={require("../../images/profile/Me5.jpg")}
-                        style={{
-                          width: 100,
-                          height: 100,
-                          borderRadius: "50%",
-                          backgroundColor: "black",
-                        }}
-                        className="avatar p-1 shadow-lg mb-4"
-                      />
-                      {/* <img
+                  <img
+                    src={require("../../images/profile/Me5.jpg")}
+                    style={{
+                      width: 100,
+                      height: 100,
+                      borderRadius: "50%",
+                      backgroundColor: "black",
+                    }}
+                    className="avatar p-1 shadow-lg mb-4"
+                  />
+                  {/* <img
               id="imagAvatar"
               src={require("../../images/BeeingCreative5.png")}
               style={{ width: 50, height: 50, color: "#111111" }}
               className="avatar p-1 mb-4"
             /> */}
-                    </div>
-                  </div>
-                  <div className="col-12">
-                    <h2 className="text-left" style={{ color: "#dddddd" }}>
-                      A Moment to Myself
-                      {/* <span className="text-white" style={{ fontSize: "10px" }}>
+                </div>
+              </div>
+              <div className="col-12">
+                <h2 className="text-left" style={{ color: "#dddddd" }}>
+                  A Moment to Myself
+                  {/* <span className="text-white" style={{ fontSize: "10px" }}>
                         - <strong>a deadmau5 song title</strong>{" "}
                       </span> */}
-                    </h2>
+                </h2>
+              </div>
+              <div className="col-12 ">
+                <h6
+                  style={{
+                    fontSize: 15,
+                    color: "#eeeeee",
+                    textAlign: "justify",
+                  }}
+                >
+                  Starting with the curiousness and the ambitious to make
+                  something for my own when I started collage, now beside my
+                  other activities, have become part of my daily as a developer,
+                  I always consider myself as a begginer when I started doing
+                  something or trying to learn some new concepts, Experimental
+                  projects like the browser automation has been a good way for
+                  me to learn how this stuff works, in my journey of learning
+                  and collage I could complement the science of it and get
+                  knowledge of how many busnesses incorporate diffrent aspects
+                  of many kinds of technologies for specific reasons in
+                  particular following along with best practices. This is just
+                  the begining of the journey...
+                </h6>
+              </div>
+              <div className="col-12">
+                <h4 className="mt-4 text-white">Lenguages</h4>
+                <hr />
+                <div className="row">
+                  <div className="col-4" style={this.CSharpSt}>
+                    <ion-icon name="logo-windows"></ion-icon>
+                    CSharp (C#)
                   </div>
-                  <div className="col-12 ">
-                    <h6 style={{ fontSize: 15, color: "#eeeeee" }}>
-                      Starting with the curiousness and the ambitious to make
-                      something for my own when I started collage, now beside my
-                      other activities, have become part of my daily as a
-                      developer, I always consider myself as a begginer when I
-                      started doing something or trying to learn some new
-                      concepts, Experimental projects like the browser
-                      automation has been a good way for me to learn how this
-                      stuff works, in my journey of learning and collage I could
-                      complement the science of it and get knowledge of how many
-                      busnesses incorporate diffrent aspects of many kinds of
-                      technologies for specific reasons in particular following
-                      along with best practices. This is just the begining of
-                      the journey...
-                    </h6>
+                  <div className="col">
+                    <div
+                      id="level"
+                      style={{
+                        width: "20px",
+                        height: "20px",
+                        backgroundColor: "#22c9dc",
+                      }}
+                    />
                   </div>
-                  <div className="col-12">
-                    <h4 className="mt-4 text-white">Lenguages</h4>
-                    <hr />
-                    <div className="row">
-                      <div className="col-4" style={this.CSharpSt}>
-                        <ion-icon name="logo-windows"></ion-icon>
-                        CSharp (C#)
-                      </div>
-                      <div className="col">
-                        <div
-                          id="level"
-                          style={{
-                            width: "20px",
-                            height: "20px",
-                            backgroundColor: "#22c9dc",
-                          }}
-                        />
-                      </div>
-                      <div className="col-1" style={this.CSharpSt}>
-                        75%
-                      </div>
-                    </div>
-                    <hr />
-                    <div className="row">
-                      <div className="col-4" style={this.PythonSt}>
-                        <ion-icon name="logo-python"></ion-icon>
-                        Python
-                      </div>
-                      <div className="col">
-                        <div
-                          id="level2"
-                          style={{
-                            width: "20px",
-                            height: "20px",
-                            backgroundColor: "#FD8922",
-                          }}
-                        />
-                      </div>
-                      <div className="col-1" style={this.PythonSt}>
-                        65%
-                      </div>
-                    </div>
-                    <hr />
-                    <div className="row">
-                      <div className="col-4" style={this.JavaScriptSt}>
-                        <ion-icon name="logo-javascript"></ion-icon>
-                        JavaScript
-                      </div>
-                      <div className="col">
-                        <div
-                          id="level3"
-                          style={{
-                            width: "20px",
-                            height: "20px",
-                            backgroundColor: "#ffe539",
-                          }}
-                        />
-                      </div>
-                      <div className="col-1" style={this.JavaScriptSt}>
-                        60%
-                      </div>
-                    </div>
+                  <div className="col-1" style={this.CSharpSt}>
+                    75%
                   </div>
                 </div>
-              </dib>
-              <div className="col-1"></div>
+                <hr />
+                <div className="row">
+                  <div className="col-4" style={this.PythonSt}>
+                    <ion-icon name="logo-python"></ion-icon>
+                    Python
+                  </div>
+                  <div className="col">
+                    <div
+                      id="level2"
+                      style={{
+                        width: "20px",
+                        height: "20px",
+                        backgroundColor: "#FD8922",
+                      }}
+                    />
+                  </div>
+                  <div className="col-1" style={this.PythonSt}>
+                    65%
+                  </div>
+                </div>
+                <hr />
+                <div className="row">
+                  <div className="col-4" style={this.JavaScriptSt}>
+                    <ion-icon name="logo-javascript"></ion-icon>
+                    JavaScript
+                  </div>
+                  <div className="col">
+                    <div
+                      id="level3"
+                      style={{
+                        width: "20px",
+                        height: "20px",
+                        backgroundColor: "#ffe539",
+                      }}
+                    />
+                  </div>
+                  <div className="col-1" style={this.JavaScriptSt}>
+                    60%
+                  </div>
+                </div>
+              </div>
             </div>
+
             {/* <div
               className="row align-items-center justify-content-center text-dark pt-5 pb-5"
               style={{ height: "550px", overflowY: "auto" }}
